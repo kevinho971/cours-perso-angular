0f37a97b8fca32474cc28ae78603c5cdc6f448c1
Kevin <morand.kevin55@gmail.com>
2018-04-25 23:44:12 +0200

    MAJ app module afin d'implémenter Firebase et devis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {AngularFireModule, FirebaseAppConfig} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
+import { DevisComponent } from './devis/devis.component';
+
+
+const CONFIG: FirebaseAppConfig = {
+  apiKey: "AIzaSyCgbXYniMNTqKBerXSu1M3LCy8BAyQbGSk",
+  authDomain: "udemy-angular-df016.firebaseapp.com",
+  databaseURL: "https://udemy-angular-df016.firebaseio.com",
+  projectId: "udemy-angular-df016",
+  storageBucket: "udemy-angular-df016.appspot.com",
+  messagingSenderId: "172311920418"
+};
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DevisComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(CONFIG),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
