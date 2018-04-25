9b9193c2c2baf09ebd2ec0dfecd0415ed8d77b94
Kevin <morand.kevin55@gmail.com>
2018-04-25 23:43:17 +0200

    Modification Component devis afin d'achier list devis

--- src/app/devis/devis.component.ts
+++ src/app/devis/devis.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import {AngularFireDatabase} from 'angularfire2/database';
+
 @Component({
   selector: 'app-devis',
   templateUrl: './devis.component.html',
   styleUrls: ['./devis.component.css']
 })
 export class DevisComponent implements OnInit {
-
-  constructor() { }
+  devis$;
+  
+  constructor(private afDb: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.devis$ = this.afDb.list('devis').valueChanges();
   }
 
 }
